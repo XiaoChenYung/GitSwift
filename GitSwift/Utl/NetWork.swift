d761d38dbecf0b72ecea9407d88b8e79a8812ec3
杨晓晨 <xiaochenyung@gmail.com>
2017-07-20 17:43:14 +0800

    init net

---
+++ GitSwift/Utl/NetWork.swift
@@ -0,0 +1,19 @@
+//
+//  NetWork.swift
+//  GitSwift
+//
+//  Created by tm on 2017/7/19.
+//  Copyright © 2017年 tm. All rights reserved.
+//
+
+import Foundation
+import RxAlamofire
+import Alamofire
+
+enum NetMethod {
+    case get
+}
+
+struct NetWork {
+    
+}
---
+++ GitSwift/View/View.swift
@@ -0,0 +1,9 @@
+//
+//  View.swift
+//  GitSwift
+//
+//  Created by tm on 2017/7/19.
+//  Copyright © 2017年 tm. All rights reserved.
+//
+
+import Foundation
